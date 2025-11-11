f367b77d1d715fc589642cecb108155c8e389d8e
ibnuhibbann <ibnuhiban007@gmail.com>
2025-11-12 04:34:49 +0800

    project all

--- project.js
+++ project.js
@@ -2,6 +2,20 @@
 const modal = document.getElementById("imgModal");
 const modalImg = document.getElementById("modalImg");
 const captionText = document.getElementById("caption");
+const btn = document.getElementById('toggle-desc');
+const content = document.getElementById('desc-content');
+
+btn.addEventListener('click', () => {
+    content.classList.toggle('expanded');
+    btn.classList.toggle('up');
+
+    // ganti arrow
+    if (content.classList.contains('expanded')) {
+        btn.innerHTML = "&#x25B2;"; // arrow up
+    } else {
+        btn.innerHTML = "&#x25BC;"; // arrow down
+    }
+});
 
 document.querySelectorAll(".zoom-img").forEach(img => {
     img.onclick = () => {
